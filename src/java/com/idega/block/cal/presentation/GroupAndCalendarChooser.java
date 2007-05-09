50a87a34e3d827135d280fe279aa560c1b671f08
Justinas Rakita <justinas@idega.com>
2007-05-09 11:23:17 +0000

    group chooser moved to user bundle

--- src/java/com/idega/block/cal/presentation/GroupAndCalendarChooser.java
+++ src/java/com/idega/block/cal/presentation/GroupAndCalendarChooser.java
@@ -1,33 +1,17 @@
 package com.idega.block.cal.presentation;
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Vector;
 
 import javax.faces.context.FacesContext;
 
 import com.idega.block.cal.business.CalendarConstants;
-import com.idega.core.data.IWTreeNode;
-import com.idega.idegaweb.IWApplicationContext;
 import com.idega.idegaweb.IWBundle;
 import com.idega.presentation.Block;
 import com.idega.presentation.IWContext;
 import com.idega.presentation.Layer;
 import com.idega.presentation.Page;
 import com.idega.presentation.PresentationObjectUtil;
-import com.idega.presentation.Table;
-import com.idega.presentation.text.Text;
-import com.idega.presentation.ui.GenericButton;
-import com.idega.presentation.ui.PasswordInput;
-import com.idega.presentation.ui.RadioButton;
-import com.idega.presentation.ui.RadioGroup;
-import com.idega.presentation.ui.TextInput;
-import com.idega.user.business.GroupTreeNode;
-import com.idega.user.business.UserBusiness;
-import com.idega.user.data.Group;
-import com.idega.webface.WFTreeNode;
+import com.idega.user.business.GroupChooser;
 /**
  * 
  * @author <a href="justinas@idega.com">Justinas Rakita</a>
@@ -35,66 +19,122 @@ import com.idega.webface.WFTreeNode;
  */
 public class GroupAndCalendarChooser extends Block {
 	
-	private UserBusiness userBiz = null;
+//	private UserBusiness userBiz = null;
 	private static IWBundle bundle = null;	
 	
 	public void main(IWContext iwc) {
+		GroupChooser chooser = new GroupChooser();
 		Page parentPage = PresentationObjectUtil.getParentPage(this);
 		if (parentPage == null){
 			return;
 		}
-		//TODO: get real resource path
-		IWBundle bundle = getBundle();
-		String path = bundle.getResourcesPath();
-		parentPage.addJavascriptURL(path + "/javascript/groupTree.js");
-		parentPage.addJavascriptURL(path + "/javascript/cal.js");
-		parentPage.addJavascriptURL("/dwr/interface/CalService.js");
-		parentPage.addJavascriptURL("/dwr/engine.js");
-		parentPage.addJavaScriptAfterJavaScriptURLs("tree", "addEvent(window, 'load', initCalendarTree)");
-		Layer layer = new Layer();
-		layer.setId(CalendarConstants.groupsDivId);
-		RadioGroup radioGroup = new RadioGroup("server");
-			
-		Text txtLocal = new Text(bundle.getLocalizedString(CalendarConstants.localServerParameterName,"Local server"));
-		RadioButton btnLocal = new RadioButton("server");
-		btnLocal.attributes.put("onClick","setLocal('"+CalendarConstants.tableID+"');");
-
-		Text txtRemote = new Text(bundle.getLocalizedString(CalendarConstants.remoteServerParameterName,"Remote server"));
-		RadioButton btnRemote = new RadioButton("server");
-		btnRemote.attributes.put("onClick","setRemote('"+CalendarConstants.tableID+"');");
-			
-		radioGroup.addRadioButton(btnLocal, txtLocal);
-		radioGroup.addRadioButton(btnRemote, txtRemote);	
-			
-		Text txtSeverName = new Text(bundle.getLocalizedString(CalendarConstants.serverNameParameterName, "Server name"));
-		Text txtLogin = new Text(bundle.getLocalizedString(CalendarConstants.loginParameterName, "Login"));
-		Text txtPass = new Text(bundle.getLocalizedString(CalendarConstants.passwordParameterName, "Password"));
-			
-		TextInput inpServerName = new TextInput();
-		inpServerName.setId(CalendarConstants.serverNameFieldId);
-		TextInput inpLogin = new TextInput();
-		inpLogin.setId(CalendarConstants.loginFieldId);
-		PasswordInput pswInput = new PasswordInput();
-		pswInput.setId(CalendarConstants.passwordFieldid);
-			
-		GenericButton button = new GenericButton(bundle.getLocalizedString(CalendarConstants.getGroupsParameterName, "Get groups"));
-		button.attributes.put("onClick", "sendConnectionData('"+CalendarConstants.serverNameFieldId+"','"+CalendarConstants.loginFieldId+"','"+CalendarConstants.passwordFieldid+"')");
-			
-		Table table = new Table(2, 4);
-		table.setId(CalendarConstants.tableID);
-		table.attributes.put("class", "hidden_table");
-		table.add(txtSeverName, 1, 1);
-		table.add(txtLogin, 1, 2);
-		table.add(txtPass, 1, 3);
-		table.add(inpServerName, 2, 1);
-		table.add(inpLogin, 2, 2);
-		table.add(pswInput, 2, 3);
-		table.add(button, 1, 4);
-			
-		layer.add(radioGroup);
-		layer.add(table);
-		parentPage.add(layer);
+		parentPage = chooser.getParentPage(parentPage);
 	}
+	
+//	public void main(IWContext iwc) {
+//		Page parentPage = PresentationObjectUtil.getParentPage(this);
+//		if (parentPage == null){
+//			return;
+//		}
+//		//TODO: get real resource path
+//		IWBundle bundle = getBundle();
+//		String path = bundle.getResourcesPath();
+//		parentPage.addJavascriptURL(path + "/javascript/groupTree.js");
+//		parentPage.addJavascriptURL(path + "/javascript/cal.js");
+//		parentPage.addJavascriptURL("/dwr/interface/CalService.js");
+//		parentPage.addJavascriptURL("/dwr/engine.js");
+////		parentPage.addJavaScriptAfterJavaScriptURLs("tree", "addEvent(window, 'load', loadTree)");
+//		parentPage.addJavaScriptAfterJavaScriptURLs("tree", "registerEvent(window, 'load', loadTree)");
+//		Layer layer = new Layer();
+//		layer.setId(CalendarConstants.groupsDivId);
+//		
+//		RadioGroup radioGroup = new RadioGroup("server");
+//			
+//		Text txtLocal = new Text(bundle.getLocalizedString(CalendarConstants.localServerParameterName,"Local server"));
+//		RadioButton btnLocal = new RadioButton("server");
+//		
+//		btnLocal.setId("radioBtnLocal");
+//		
+//		btnLocal.setOnClick("setLocal('"+CalendarConstants.tableID+"');");
+//
+//		Text txtRemote = new Text(bundle.getLocalizedString(CalendarConstants.remoteServerParameterName,"Remote server"));
+//		RadioButton btnRemote = new RadioButton("server");
+//		btnRemote.setOnClick("setRemote('"+CalendarConstants.tableID+"');");
+//		
+//		btnLocal.setId("radioBtnRemote");
+//		
+//		radioGroup.addRadioButton(btnLocal, txtLocal);
+//		radioGroup.addRadioButton(btnRemote, txtRemote);	
+//			
+//		Text txtServerName = new Text(bundle.getLocalizedString(CalendarConstants.serverNameParameterName, "Server name"));
+//		Text txtLogin = new Text(bundle.getLocalizedString(CalendarConstants.loginParameterName, "Login"));
+//		Text txtPass = new Text(bundle.getLocalizedString(CalendarConstants.passwordParameterName, "Password"));
+//			
+//		TextInput inpServerName = new TextInput();
+//		inpServerName.setId(CalendarConstants.serverNameFieldId);
+//		TextInput inpLogin = new TextInput();
+//		inpLogin.setId(CalendarConstants.loginFieldId);
+//		PasswordInput pswInput = new PasswordInput();
+//		pswInput.setId(CalendarConstants.passwordFieldid);
+//			
+//		GenericButton button = new GenericButton(bundle.getLocalizedString(CalendarConstants.refreshParameterName, "Refresh"));
+//		button.setOnClick("sendConnectionData('"+CalendarConstants.serverNameFieldId+"','"+CalendarConstants.loginFieldId+"','"+CalendarConstants.passwordFieldid+"')");
+//		button.setId("connDataBtn");
+////		button.attributes.put("onClick", "sendConnectionData('"+CalendarConstants.serverNameFieldId+"','"+CalendarConstants.loginFieldId+"','"+CalendarConstants.passwordFieldid+"')");
+//			
+////		Table table = new Table(2, 4);
+////		table.setId(CalendarConstants.tableID);
+////		table.attributes.put("class", "hidden_table");
+////		table.add(txtServerName, 1, 1);
+////		table.add(txtLogin, 1, 2);
+////		table.add(txtPass, 1, 3);
+////		table.add(inpServerName, 2, 1);
+////		table.add(inpLogin, 2, 2);
+////		table.add(pswInput, 2, 3);
+////		table.add(button, 1, 4);
+//		
+//		Layer connData = new Layer();
+//		connData.setId("connectionData");
+////		connData.setStyleClass("hidden_table");
+//		Layer serverNameLayer = new Layer();
+//		serverNameLayer.add(txtServerName);
+//		serverNameLayer.add(inpServerName);
+//		serverNameLayer.setStyleClass("connection_table_row");
+//		
+//		Layer loginLayer = new Layer();
+//		loginLayer.add(txtLogin);
+//		loginLayer.add(inpLogin);
+//		loginLayer.setStyleClass("connection_table_row");
+//		
+//		Layer passLayer = new Layer();	
+//		passLayer.add(txtPass);
+//		passLayer.add(pswInput);		
+//		passLayer.setStyleClass("connection_table_row");
+//		
+//		Layer buttonLayer = new Layer();
+//		buttonLayer.setStyleClass("connection_table_button");
+//		buttonLayer.add(button);
+//		
+//		
+//		connData.add(serverNameLayer);
+//		connData.add(loginLayer);
+//		connData.add(passLayer);
+//		connData.add(buttonLayer);
+//		
+//		connData.setStyleClass("hidden_table");
+//		
+////		layer.add(connData);
+//		layer.add(radioGroup);
+////		layer.add(table);
+//		layer.add(connData);
+//		
+//		Script script = new Script();
+//		script.addScriptLine("setDivId(\'"+CalendarConstants.groupsDivId+"\');");
+//		layer.add(script);
+////		this.getChildren().add(script);		
+//		
+//		parentPage.add(layer);
+//	}
 	
 	public void encodeBegin(FacesContext fc)throws IOException{
 		super.encodeBegin(fc);
@@ -104,17 +144,17 @@ public class GroupAndCalendarChooser extends Block {
 		
 	}
 	
-	public UserBusiness getUserBusiness(IWApplicationContext iwc) {
-		if (this.userBiz == null) {
-			try {
-				this.userBiz = (UserBusiness) com.idega.business.IBOLookup.getServiceInstance(iwc, UserBusiness.class);
-			}
-			catch (java.rmi.RemoteException rme) {
-				throw new RuntimeException(rme.getMessage());
-			}
-		}
-		return this.userBiz;
-	}	
+//	public UserBusiness getUserBusiness(IWApplicationContext iwc) {
+//		if (this.userBiz == null) {
+//			try {
+//				this.userBiz = (UserBusiness) com.idega.business.IBOLookup.getServiceInstance(iwc, UserBusiness.class);
+//			}
+//			catch (java.rmi.RemoteException rme) {
+//				throw new RuntimeException(rme.getMessage());
+//			}
+//		}
+//		return this.userBiz;
+//	}	
 	
 //	private Collection getGroupTypes(String selectedGroup, IWContext iwc)  {
 //		Collection groupTypes = new ArrayList();
@@ -159,32 +199,32 @@ public class GroupAndCalendarChooser extends Block {
 //		return false;
 //	}
 	
-	public Collection convertGroupCollectionToGroupNodeCollection(Collection col, IWApplicationContext iwac){
-		List list = new Vector();
-		
-		Iterator iter = col.iterator();
-		while (iter.hasNext()) {
-			Group group = (Group) iter.next();
-			GroupTreeNode node = new GroupTreeNode(group,iwac);
-			list.add(node);
-		}
-
-		return list;
-	}	
+//	public Collection convertGroupCollectionToGroupNodeCollection(Collection col, IWApplicationContext iwac){
+//		List list = new Vector();
+//		
+//		Iterator iter = col.iterator();
+//		while (iter.hasNext()) {
+//			Group group = (Group) iter.next();
+//			GroupTreeNode node = new GroupTreeNode(group,iwac);
+//			list.add(node);
+//		}
+//
+//		return list;
+//	}	
 	
-	public WFTreeNode getRootNode(Collection groupNodes, WFTreeNode rootNode){
-//		WFTreeNode node = null;
-		for (Iterator iter = groupNodes.iterator(); iter.hasNext();) {
-			GroupTreeNode element = (GroupTreeNode) iter.next();
-			WFTreeNode newNode = new WFTreeNode(new IWTreeNode(element.getNodeName()));				
-			if(element.getChildCount() != 0)
-				newNode = getRootNode(element.getChildren(), newNode);
-			newNode.setIdentifier(element.getId());
-			rootNode.addChild(newNode);			
-		}
-		
-		return rootNode;
-	}
+//	public WFTreeNode getRootNode(Collection groupNodes, WFTreeNode rootNode){
+////		WFTreeNode node = null;
+//		for (Iterator iter = groupNodes.iterator(); iter.hasNext();) {
+//			GroupTreeNode element = (GroupTreeNode) iter.next();
+//			WFTreeNode newNode = new WFTreeNode(new IWTreeNode(element.getNodeName()));				
+//			if(element.getChildCount() != 0)
+//				newNode = getRootNode(element.getChildren(), newNode);
+//			newNode.setIdentifier(element.getId());
+//			rootNode.addChild(newNode);			
+//		}
+//		
+//		return rootNode;
+//	}
 	
 	public static IWBundle getBundle() {
 		if (bundle == null) {

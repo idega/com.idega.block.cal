aee15fde050be594b1721d5be35995499b7197e6
Thorhallur Helgason <laddi@idega.com>
2007-05-30 08:29:45 +0000

    Declaring new block version.

--- src/java/com/idega/block/cal/business/CalServiceHomeImpl.java
+++ src/java/com/idega/block/cal/business/CalServiceHomeImpl.java
@@ -1,17 +1,10 @@
 package com.idega.block.cal.business;
 
-import java.rmi.RemoteException;
-
 import javax.ejb.CreateException;
-import javax.ejb.EJBMetaData;
-import javax.ejb.Handle;
-import javax.ejb.HomeHandle;
-import javax.ejb.RemoveException;
 
 import com.idega.business.IBOHomeImpl;
 
 public class CalServiceHomeImpl extends IBOHomeImpl implements CalServiceHome {
-
 
 	private static final long serialVersionUID = 7141978995132493723L;
 

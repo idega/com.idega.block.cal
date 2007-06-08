27645ee198f2f3ff9f78dac179ec201d406a4383
Justinas Rakita <justinas@idega.com>
2007-06-08 09:34:20 +0000

    *** empty log message ***

--- src/java/com/idega/block/cal/presentation/CalendarChooser.java
+++ src/java/com/idega/block/cal/presentation/CalendarChooser.java
@@ -1,7 +1,6 @@
 package com.idega.block.cal.presentation;
 
 import com.idega.presentation.ui.AbstractChooser;
-import com.idega.user.presentation.group.GroupInfoChooserBlock;
 
 public class CalendarChooser  extends AbstractChooser{
 	

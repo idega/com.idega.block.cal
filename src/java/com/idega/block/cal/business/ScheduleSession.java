50f26ce2f2bcebb11d03045a2f57254703edfef9
Eirikur Hrafnsson <eiki@idega.com>
2007-06-20 15:53:38 +0000

    regenerated helper classes

--- src/java/com/idega/block/cal/business/ScheduleSession.java
+++ src/java/com/idega/block/cal/business/ScheduleSession.java
@@ -1,23 +1,49 @@
-
 package com.idega.block.cal.business;
 
+
 import java.util.List;
-
 import org.jdom.Document;
-
 import com.idega.business.IBOSession;
+import java.rmi.RemoteException;
 
-public interface ScheduleSession extends IBOSession{
+public interface ScheduleSession extends IBOSession {
+	/**
+	 * @see com.idega.block.cal.business.ScheduleSessionBean#changeModeToDay
+	 */
+	public Document changeModeToDay(String id) throws RemoteException;
 
-	public Document getSchedule(String id, List<ScheduleEntry> result, boolean usePreviousEntries);
-	public Document changeModeToDay(String id);
-	public Document changeModeToWorkweek(String id);			
-	public Document changeModeToWeek(String id);			
-	public Document changeModeToMonth(String id);			
-	public Document getNext(String id);
-	public Document getPrevious(String id);	
-	public Document getScheduleDOM(List<ScheduleEntry> entries, String scheduleId);
-	
-	//to CalService
-	
+	/**
+	 * @see com.idega.block.cal.business.ScheduleSessionBean#changeModeToWorkweek
+	 */
+	public Document changeModeToWorkweek(String id) throws RemoteException;
+
+	/**
+	 * @see com.idega.block.cal.business.ScheduleSessionBean#changeModeToWeek
+	 */
+	public Document changeModeToWeek(String id) throws RemoteException;
+
+	/**
+	 * @see com.idega.block.cal.business.ScheduleSessionBean#changeModeToMonth
+	 */
+	public Document changeModeToMonth(String id) throws RemoteException;
+
+	/**
+	 * @see com.idega.block.cal.business.ScheduleSessionBean#getNext
+	 */
+	public Document getNext(String id) throws RemoteException;
+
+	/**
+	 * @see com.idega.block.cal.business.ScheduleSessionBean#getPrevious
+	 */
+	public Document getPrevious(String id) throws RemoteException;
+
+	/**
+	 * @see com.idega.block.cal.business.ScheduleSessionBean#getSchedule
+	 */
+	public Document getSchedule(String id, List<ScheduleEntry> result, boolean usePreviousEntries) throws RemoteException;
+
+	/**
+	 * @see com.idega.block.cal.business.ScheduleSessionBean#getScheduleDOM
+	 */
+	public Document getScheduleDOM(List<ScheduleEntry> entries, String id) throws RemoteException;
 }
--- src/java/com/idega/block/cal/business/ScheduleSessionHomeImpl.java
+++ src/java/com/idega/block/cal/business/ScheduleSessionHomeImpl.java
@@ -1,14 +1,10 @@
 package com.idega.block.cal.business;
 
-import javax.ejb.CreateException;
 
+import javax.ejb.CreateException;
 import com.idega.business.IBOHomeImpl;
 
 public class ScheduleSessionHomeImpl extends IBOHomeImpl implements ScheduleSessionHome {
-
-
-	private static final long serialVersionUID = 7141978995132493723L;
-
 	public Class getBeanInterfaceClass() {
 		return ScheduleSession.class;
 	}
@@ -16,5 +12,4 @@ public class ScheduleSessionHomeImpl extends IBOHomeImpl implements ScheduleSess
 	public ScheduleSession create() throws CreateException {
 		return (ScheduleSession) super.createIBO();
 	}
-
 }

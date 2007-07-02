638bce8d3e6dc38385fd7b8c3c7d350d7f4fa379
Justinas Rakita <justinas@idega.com>
2007-07-02 08:16:26 +0000

    removed unnecessary comments

--- src/java/com/idega/block/cal/data/CalendarManagerBean.java
+++ src/java/com/idega/block/cal/data/CalendarManagerBean.java
@@ -8,16 +8,9 @@ import com.idega.user.bean.GroupsAndCalendarPropertiesBean;
 
 public class CalendarManagerBean implements Serializable {
 
-//	private Map<String, PropertiesBean> abstractProperties = null;
-//	private Map<String, GroupPropertiesBean> groupProperties = null;
-//	private Map<String, UserPropertiesBean> userProperties = null;
-	
 	private Map<String, GroupsAndCalendarPropertiesBean> calendarProperties = null;
 	
 	public CalendarManagerBean() {
-//		abstractProperties = new HashMap<String, PropertiesBean>();
-//		groupProperties = new HashMap<String, GroupPropertiesBean>();
-//		userProperties = new HashMap<String, UserPropertiesBean>();
 		calendarProperties = new HashMap<String, GroupsAndCalendarPropertiesBean>();
 	}
 	
@@ -45,84 +38,5 @@ public class CalendarManagerBean implements Serializable {
 		
 		return true;
 	}
-//	
-//	public boolean addAbstractProperties(String instanceId, PropertiesBean propertiesBean) {
-//		if (instanceId == null || propertiesBean == null) {
-//			return false;
-//		}
-//		abstractProperties.put(instanceId, propertiesBean);
-//		return true;
-//	}
-	
-//	private PropertiesBean getAbstractProperties(String instanceId) {
-//		if (instanceId == null) {
-//			return null;
-//		}
-//		return abstractProperties.get(instanceId);
-//	}
-	
-//	public boolean addGroupProperties(String instanceId, GroupPropertiesBean propertiesBean) {
-//		if (instanceId == null || propertiesBean == null) {
-//			return false;
-//		}
-//		groupProperties.put(instanceId, propertiesBean);
-//		return true;
-//	}
-//	
-//	public boolean addUserProperties(String instanceId, UserPropertiesBean propertiesBean) {
-//		if (instanceId == null || propertiesBean == null) {
-//			return false;
-//		}
-//		userProperties.put(instanceId, propertiesBean);
-//		return true;
-//	}
-//	
-//	public GroupPropertiesBean getGroupProperties(String instanceId) {
-//		if (instanceId == null) {
-//			return null;
-//		}
-//		PropertiesBean bean = getAbstractProperties(instanceId);
-//		if (bean == null) {
-//			return groupProperties.get(instanceId);
-//		}
-//		else {
-//			abstractProperties.remove(instanceId);
-//			return new GroupPropertiesBean(bean);
-//		}
-//		
-//	}
-	
-//	public UserPropertiesBean getUserProperties(String instanceId) {
-//		if (instanceId == null) {
-//			return null;
-//		}
-//		PropertiesBean bean = getAbstractProperties(instanceId);
-//		if (bean == null) {
-//			return userProperties.get(instanceId);
-//		}
-//		else {
-//			abstractProperties.remove(instanceId);
-//			return new UserPropertiesBean(bean);
-//		}
-//	}
-//	
-//	public boolean removeGroupProperties(String instanceId) {
-//		return removeProperties(instanceId, groupProperties);
-//	}
-//	
-//	public boolean removeUserProperties(String instanceId) {
-//		return removeProperties(instanceId, userProperties);
-//	}
-//	
-//	private boolean removeProperties(String key, Map properties) {
-//		if (key == null) {
-//			return false;
-//		}
-//		
-//		properties.remove(key);
-//		
-//		return true;
-//	}
-
 }
 

5ac67ebf1a31a0843d61160fadb3cc6cf13d1a86
Justinas Rakita <justinas@idega.com>
2007-07-02 08:19:17 +0000

    removed unnecessary source code

--- src/java/com/idega/block/cal/presentation/GroupAndCalendarChooser.java
+++ src/java/com/idega/block/cal/presentation/GroupAndCalendarChooser.java
@@ -18,9 +18,9 @@ import com.idega.presentation.ui.AbstractChooser;
  */
 public class GroupAndCalendarChooser extends AbstractChooser {
 
-	private static final String GROUPS_AND_CALENDAR_STYLE_CLASS = "groupsAndCalendarsLayerStyleClass";
-	private static final String CALENDAR_STYLE_CLASS = "calendarsLayerStyleClass";
-	private static final String NODE_ON_CLICK_ACTION = "function(){ScheduleSession.getCalendarParameters(this.id, displayCalendarAttributes);saveProperties();}";
+//	private static final String GROUPS_AND_CALENDAR_STYLE_CLASS = "groupsAndCalendarsLayerStyleClass";
+//	private static final String CALENDAR_STYLE_CLASS = "calendarsLayerStyleClass";
+//	private static final String NODE_ON_CLICK_ACTION = "function(){ScheduleSession.getCalendarParameters(this.id, displayCalendarAttributes);saveProperties();}";
 //	private UserBusiness userBiz = null;
 	private static IWBundle bundle = null;	
 	private static final String chooserHelper = "groups_and_calendar_chooser_helper";
@@ -33,28 +33,6 @@ public class GroupAndCalendarChooser extends AbstractChooser {
 		setMethod(method);
 	}
 	
-//	public void main(IWContext iwc) {
-//		Layer groupsAndCalendarsLayer = new Layer();
-////		Page parentPage = getParentPage();
-//		
-//		GroupsChooserBlock groupsChooser = new GroupsChooserBlock();
-//		groupsChooser.setNodeOnClickAction(NODE_ON_CLICK_ACTION);
-//		groupsChooser.setStyleClass("groupsLayerStyleClass");
-//		
-////		groupsAndCalendarsLayer.add(groupsChooser);
-//		
-//		CoreUtil.addJavaSciptForChooser(iwc);
-//		
-////		groupsAndCalendarsLayer.add(new CalendarChooserBlock());	
-//		CalendarChooserBlock calendarChooserBlock = new CalendarChooserBlock();
-//		calendarChooserBlock.setStyleClass(CALENDAR_STYLE_CLASS);
-//		groupsAndCalendarsLayer.add(calendarChooserBlock);		
-//		groupsAndCalendarsLayer.setStyleClass(GROUPS_AND_CALENDAR_STYLE_CLASS);
-//		
-//		add(groupsAndCalendarsLayer);
-//		
-//	}
-		
 	public void encodeBegin(FacesContext fc)throws IOException{
 		super.encodeBegin(fc);
 		Layer panels = (Layer)this.getFacet("PANELS");
@@ -62,23 +40,11 @@ public class GroupAndCalendarChooser extends AbstractChooser {
 		
 	}
 	
-//	public static IWBundle getBundle() {
-//		if (bundle == null) {
-//			setupBundle();
-//		}
-//		return bundle;
-//	}
-
 	private static void setupBundle() {
 		FacesContext context = FacesContext.getCurrentInstance();
 		IWContext iwContext = IWContext.getIWContext(context);
 		bundle = iwContext.getIWMainApplication().getBundle(CalendarConstants.BUNDLE_IDENTIFIER);
 	}
-//	@Override
-//	public boolean getChooserAttributes() {
-//		// TODO Auto-generated method stub
-//		return false;
-//	}
 	@Override
 	protected String getChooserHelperVarName() {
 		// TODO Auto-generated method stub
@@ -90,11 +56,7 @@ public class GroupAndCalendarChooser extends AbstractChooser {
 		return GroupAndCalendarChooserBlock.class;
 	}	
 	public PresentationObject getChooser(IWContext iwc, IWBundle bundle) {
-//		GroupChooserBlock chooserBlock = new GroupChooserBlock();
 		GroupAndCalendarChooserBlock chooserBlock = new GroupAndCalendarChooserBlock();
-//		chooserBlock.setGroupsChooserBlock
-//		chooserBlock.setCalChooserBlock
-		
 		return chooserBlock;
 	}
 	

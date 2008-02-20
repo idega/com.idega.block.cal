83b458c6314ffb4e501acee4e431ead5836db117
Grimur Jonsson <gimmi@idega.com>
2008-02-20 10:12:47 +0000

    debug things

--- src/java/com/idega/block/cal/business/CalBusinessBean.java
+++ src/java/com/idega/block/cal/business/CalBusinessBean.java
@@ -134,6 +134,8 @@ public class CalBusinessBean extends IBOServiceBean implements CalBusiness,UserG
 			t2.stop();
 			System.out.print("entries ("+t.getTimeString()+").");
 			System.out.println(" total time : "+t2.getTimeString());
+			System.out.println("GroupIDs used = "+groupsIds);
+			System.out.println("Ledgers used  = "+ledgersIds);
 			return returner;
 
 		} catch(Exception e) {

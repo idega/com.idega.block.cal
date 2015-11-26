df5063fd7805e1d43af5b4539a3b96f9d7ca37b2
lapiukshtiss <martynas.stake@gmail.com>
2015-11-26 16:21:47 +0200

    Method for updating calendar entry gruops

--- src/java/com/idega/block/cal/data/CalendarEntryGroupHome.java
+++ src/java/com/idega/block/cal/data/CalendarEntryGroupHome.java
@@ -5,8 +5,33 @@ import java.util.Collection;
 
 public interface CalendarEntryGroupHome extends com.idega.data.IDOHome
 {
- public CalendarEntryGroup create() throws javax.ejb.CreateException;
- public CalendarEntryGroup findByPrimaryKey(Object pk) throws javax.ejb.FinderException;
- public Collection findEntryGroupsByLedgerID(int ledgerID) throws javax.ejb.FinderException;
+	public CalendarEntryGroup create() throws javax.ejb.CreateException;
 
+	/**
+	 * 
+	 * @param pk is {@link CalendarEntryGroup#getPrimaryKey()}, not <code>null</code>;
+	 * @return entity or <code>null</code> on failure;
+	 * @author <a href="mailto:martynas@idega.is">Martynas Stakė</a>
+	 */
+	public CalendarEntryGroup findByPrimaryKey(Object pk);
+
+	public Collection findEntryGroupsByLedgerID(int ledgerID)
+			throws javax.ejb.FinderException;
+
+	/**
+	 * 
+	 * <p>Creates or updates entity</p>
+	 * @param primaryKey is {@link CalendarEntryGroup#getPrimaryKey()}, updating
+	 * is done, when not <code>null</code>;
+	 * @param name is name of recurrence, one of:
+	 * <li>none</li>
+	 * <li>daily</li>
+	 * <li>weekly</li>
+	 * <li>monthly</li>
+	 * <li>yearly</li>
+	 * @param ledgerId
+	 * @return entity or <code>null</code> on failure;
+	 * @author <a href="mailto:martynas@idega.is">Martynas Stakė</a>
+	 */
+	CalendarEntryGroup update(Integer primaryKey, String name, Integer ledgerId);
 }
--- src/java/com/idega/block/cal/data/CalendarEntryGroupHomeImpl.java
+++ src/java/com/idega/block/cal/data/CalendarEntryGroupHomeImpl.java
@@ -1,25 +1,84 @@
 package com.idega.block.cal.data;
 
 import java.util.Collection;
+import java.util.logging.Level;
+
+import javax.ejb.CreateException;
+import javax.ejb.FinderException;
+
+import com.idega.data.IDOStoreException;
 
 
 
-public class CalendarEntryGroupHomeImpl extends com.idega.data.IDOFactory implements CalendarEntryGroupHome
-{
- protected Class getEntityInterfaceClass(){
-  return CalendarEntryGroup.class;
- }
+public class CalendarEntryGroupHomeImpl extends com.idega.data.IDOFactory implements CalendarEntryGroupHome {
 
+	private static final long serialVersionUID = -3379199403679533775L;
 
- public CalendarEntryGroup create() throws javax.ejb.CreateException{
-  return (CalendarEntryGroup) super.createIDO();
- }
+	@Override
+	public CalendarEntryGroup update(Integer primaryKey, String name, Integer ledgerId) {
+		CalendarEntryGroup entity = findByPrimaryKey(primaryKey);
+		if (entity == null) {
+			entity = create();
+		}
 
+		entity.setName(name);
 
- public CalendarEntryGroup findByPrimaryKey(Object pk) throws javax.ejb.FinderException{
-  return (CalendarEntryGroup) super.findByPrimaryKeyIDO(pk);
- }
- 
+		if (ledgerId != null && ledgerId > -1) {
+			entity.setLedgerID(ledgerId);
+		}
+		
+		try {
+			entity.store();
+			return entity;
+		} catch (IDOStoreException e) {
+			java.util.logging.Logger.getLogger(getClass().getName()).log(
+					Level.WARNING, "Failed to store " + getEntityInterfaceClass() 
+							+ " cause of: ", e);
+			return null;
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.idega.data.IDOFactory#getEntityInterfaceClass()
+	 */
+	protected Class getEntityInterfaceClass() {
+		return CalendarEntryGroup.class;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.idega.block.cal.data.CalendarEntryGroupHome#create()
+	 */
+	public CalendarEntryGroup create() {
+		try {
+			return (CalendarEntryGroup) super.createIDO();
+		} catch (CreateException e) {
+			java.util.logging.Logger.getLogger(getClass().getName()).log(
+					Level.WARNING, "Failed to create " + getEntityInterfaceClass() 
+							+ " cause of: ", e);
+			return null;
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.idega.block.cal.data.CalendarEntryGroupHome#findByPrimaryKey(java.lang.Object)
+	 */
+	public CalendarEntryGroup findByPrimaryKey(Object pk){
+		if (pk != null) {
+			try {
+				return (CalendarEntryGroup) super.findByPrimaryKeyIDO(pk);
+			} catch (FinderException e) {
+				java.util.logging.Logger.getLogger(getClass().getName()).log(
+						Level.WARNING, "Failed to get " + getEntityInterfaceClass() 
+								+ "by primary key: " + pk);
+			}
+		}
+
+		return null;
+	}
+
  public Collection findEntryGroupsByLedgerID(int ledgerID) throws javax.ejb.FinderException{
  	com.idega.data.IDOEntity entity = this.idoCheckOutPooledEntity();
  	java.util.Collection ids = ((CalendarEntryGroupBMPBean)entity).ejbFindEntryGroupByLedgerID(ledgerID);

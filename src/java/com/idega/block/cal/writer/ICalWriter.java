1d83c7fd79ff64113358e91ab9b27116c9870976
lapiukshtiss <martynas.stake@gmail.com>
2015-12-29 12:27:18 +0200

    Calendar event writer to iCalendar from idega event

---
+++ src/java/com/idega/block/cal/writer/ICalWriter.java
@@ -0,0 +1,264 @@
+/**
+ * @(#)ICalWriter.java    1.0.0 14:53:43
+ *
+ * Idega Software hf. Source Code Licence Agreement x
+ *
+ * This agreement, made this 10th of February 2006 by and between 
+ * Idega Software hf., a business formed and operating under laws 
+ * of Iceland, having its principal place of business in Reykjavik, 
+ * Iceland, hereinafter after referred to as "Manufacturer" and Agura 
+ * IT hereinafter referred to as "Licensee".
+ * 1.  License Grant: Upon completion of this agreement, the source 
+ *     code that may be made available according to the documentation for 
+ *     a particular software product (Software) from Manufacturer 
+ *     (Source Code) shall be provided to Licensee, provided that 
+ *     (1) funds have been received for payment of the License for Software and 
+ *     (2) the appropriate License has been purchased as stated in the 
+ *     documentation for Software. As used in this License Agreement, 
+ *     Licensee shall also mean the individual using or installing 
+ *     the source code together with any individual or entity, including 
+ *     but not limited to your employer, on whose behalf you are acting 
+ *     in using or installing the Source Code. By completing this agreement, 
+ *     Licensee agrees to be bound by the terms and conditions of this Source 
+ *     Code License Agreement. This Source Code License Agreement shall 
+ *     be an extension of the Software License Agreement for the associated 
+ *     product. No additional amendment or modification shall be made 
+ *     to this Agreement except in writing signed by Licensee and 
+ *     Manufacturer. This Agreement is effective indefinitely and once
+ *     completed, cannot be terminated. Manufacturer hereby grants to 
+ *     Licensee a non-transferable, worldwide license during the term of 
+ *     this Agreement to use the Source Code for the associated product 
+ *     purchased. In the event the Software License Agreement to the 
+ *     associated product is terminated; (1) Licensee's rights to use 
+ *     the Source Code are revoked and (2) Licensee shall destroy all 
+ *     copies of the Source Code including any Source Code used in 
+ *     Licensee's applications.
+ * 2.  License Limitations
+ *     2.1 Licensee may not resell, rent, lease or distribute the 
+ *         Source Code alone, it shall only be distributed as a 
+ *         compiled component of an application.
+ *     2.2 Licensee shall protect and keep secure all Source Code 
+ *         provided by this this Source Code License Agreement. 
+ *         All Source Code provided by this Agreement that is used 
+ *         with an application that is distributed or accessible outside
+ *         Licensee's organization (including use from the Internet), 
+ *         must be protected to the extent that it cannot be easily 
+ *         extracted or decompiled.
+ *     2.3 The Licensee shall not resell, rent, lease or distribute 
+ *         the products created from the Source Code in any way that 
+ *         would compete with Idega Software.
+ *     2.4 Manufacturer's copyright notices may not be removed from 
+ *         the Source Code.
+ *     2.5 All modifications on the source code by Licencee must 
+ *         be submitted to or provided to Manufacturer.
+ * 3.  Copyright: Manufacturer's source code is copyrighted and contains 
+ *     proprietary information. Licensee shall not distribute or 
+ *     reveal the Source Code to anyone other than the software 
+ *     developers of Licensee's organization. Licensee may be held 
+ *     legally responsible for any infringement of intellectual property 
+ *     rights that is caused or encouraged by Licensee's failure to abide 
+ *     by the terms of this Agreement. Licensee may make copies of the 
+ *     Source Code provided the copyright and trademark notices are 
+ *     reproduced in their entirety on the copy. Manufacturer reserves 
+ *     all rights not specifically granted to Licensee.
+ *
+ * 4.  Warranty & Risks: Although efforts have been made to assure that the 
+ *     Source Code is correct, reliable, date compliant, and technically 
+ *     accurate, the Source Code is licensed to Licensee as is and without 
+ *     warranties as to performance of merchantability, fitness for a 
+ *     particular purpose or use, or any other warranties whether 
+ *     expressed or implied. Licensee's organization and all users 
+ *     of the source code assume all risks when using it. The manufacturers, 
+ *     distributors and resellers of the Source Code shall not be liable 
+ *     for any consequential, incidental, punitive or special damages 
+ *     arising out of the use of or inability to use the source code or 
+ *     the provision of or failure to provide support services, even if we 
+ *     have been advised of the possibility of such damages. In any case, 
+ *     the entire liability under any provision of this agreement shall be 
+ *     limited to the greater of the amount actually paid by Licensee for the 
+ *     Software or 5.00 USD. No returns will be provided for the associated 
+ *     License that was purchased to become eligible to receive the Source 
+ *     Code after Licensee receives the source code. 
+ */
+package com.idega.block.cal.writer;
+
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import net.fortuna.ical4j.data.CalendarOutputter;
+import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.DateTime;
+import net.fortuna.ical4j.model.TimeZone;
+import net.fortuna.ical4j.model.TimeZoneRegistry;
+import net.fortuna.ical4j.model.TimeZoneRegistryFactory;
+import net.fortuna.ical4j.model.ValidationException;
+import net.fortuna.ical4j.model.component.VEvent;
+import net.fortuna.ical4j.model.component.VTimeZone;
+import net.fortuna.ical4j.model.property.CalScale;
+import net.fortuna.ical4j.model.property.Description;
+import net.fortuna.ical4j.model.property.DtEnd;
+import net.fortuna.ical4j.model.property.DtStamp;
+import net.fortuna.ical4j.model.property.DtStart;
+import net.fortuna.ical4j.model.property.Location;
+import net.fortuna.ical4j.model.property.ProdId;
+import net.fortuna.ical4j.model.property.Summary;
+import net.fortuna.ical4j.model.property.Uid;
+import net.fortuna.ical4j.model.property.Version;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import com.idega.block.cal.data.CalendarEntry;
+import com.idega.block.cal.data.CalendarEntryHome;
+import com.idega.core.business.DefaultSpringBean;
+import com.idega.data.IDOLookup;
+import com.idega.data.IDOLookupException;
+import com.idega.user.data.bean.User;
+import com.idega.util.ListUtil;
+import com.idega.util.StringUtil;
+
+/**
+ * <p>Class for writing calendar event to iCalendar file</p>
+ * <p>You can report about problems to: 
+ * <a href="mailto:martynas@idega.is">Martynas Stakė</a></p>
+ *
+ * @version 1.0.0 2015 gruod. 28
+ * @author <a href="mailto:martynas@idega.is">Martynas Stakė</a>
+ */
+@Service
+@Scope(BeanDefinition.SCOPE_SINGLETON)
+public class ICalWriter extends DefaultSpringBean {
+
+	private CalendarEntryHome calendarEntryHome;
+
+	private CalendarEntryHome getCalendarEntryHome() {
+		if (this.calendarEntryHome == null) {
+			try {
+				this.calendarEntryHome = (CalendarEntryHome) IDOLookup
+						.getHome(CalendarEntry.class);
+			} catch (IDOLookupException e) {
+				Logger.getLogger(getClass().getName()).log(Level.WARNING, 
+						"Failed to get " + CalendarEntryHome.class + " cause of: ", e);
+			}
+		}
+
+		return this.calendarEntryHome;
+	}
+
+	public Calendar newCalendar() {
+		Calendar calendar = new Calendar();
+		calendar.getProperties().add(new ProdId("-//Idega Open Source//iCal4j 1.0//EN"));
+		calendar.getProperties().add(Version.VERSION_2_0);
+		calendar.getProperties().add(CalScale.GREGORIAN);
+		return calendar;
+	}
+
+	public VEvent parseEvent(CalendarEntry entry) {
+		if (entry != null) {
+			TimeZoneRegistry registry = TimeZoneRegistryFactory.getInstance().createRegistry();
+			TimeZone timezone = registry.getTimeZone("Atlantic/Reykjavik");
+			VTimeZone tz = timezone.getVTimeZone();
+
+			VEvent event = new VEvent();
+
+			/*
+			 * Name
+			 */
+			Summary name = new Summary(entry.getName());
+			event.getProperties().add(name);
+
+			/*
+			 * Start time
+			 */
+			event.getProperties().remove(event.getDateStamp());
+
+			DateTime dateTime = new DateTime(entry.getDate());
+			DtStamp start = new DtStamp(dateTime);
+			event.getProperties().add(start);
+
+			/*
+			 * Start date
+			 */
+			event.getProperties().remove(event.getStartDate());
+
+			DtStart startDate = new DtStart(new DateTime(entry.getDate()));
+			event.getProperties().add(startDate);
+
+			/*
+			 * End date
+			 */
+			DtEnd endDate = new DtEnd(new DateTime(entry.getEndDate()));
+			event.getProperties().add(endDate);
+
+			/*
+			 * Location
+			 */
+			if (!StringUtil.isEmpty(entry.getLocation())) {
+				Location location = new Location(entry.getLocation());
+				event.getProperties().add(location);
+			}
+
+			/*
+			 * Description
+			 */
+			if (!StringUtil.isEmpty(entry.getDescription())) {
+				Description description = new Description(entry.getDescription());
+				event.getProperties().add(description);
+			}
+
+			/*
+			 * id
+			 */
+			if (!StringUtil.isEmpty(entry.getExternalEventId())) {
+				Uid uid = new Uid(entry.getPrimaryKey().toString());
+				event.getProperties().add(uid);
+			}
+
+			try {
+				event.validate();
+			} catch (ValidationException e) {
+				java.util.logging.Logger.getLogger(getClass().getName()).log(
+						Level.WARNING, "Event creation problem: ", e);
+			}
+
+			return event;
+		}
+
+		return null;
+	}
+
+	public void write(Collection<CalendarEntry> calendarEntries, OutputStream stream) {
+		if (!ListUtil.isEmpty(calendarEntries)) {
+			ArrayList<VEvent> events = new ArrayList<VEvent>();
+			for (CalendarEntry calendarEntry: calendarEntries) {
+				VEvent event = parseEvent(calendarEntry);
+				if (event != null) {
+					events.add(event);
+				}
+			}
+
+			if (!ListUtil.isEmpty(events)) {
+				Calendar calendar = newCalendar();
+				calendar.getComponents().addAll(events);
+
+				CalendarOutputter outputter = new CalendarOutputter();
+				try {
+					outputter.output(calendar, stream);
+				} catch (Exception e) {
+					java.util.logging.Logger.getLogger(getClass().getName()).log(
+							Level.WARNING, 
+							"Failed to write data to output stream, cause of: ", e);
+				}
+			}
+		}
+	}
+
+	public void write(User user, OutputStream stream) {
+		write(getCalendarEntryHome().findAll(user, null, null, null), stream);
+	}
+	
+}
